8cf9b0a4294cb40ae8718c2bcc5286826bb74787
Gabriel Guimaraes <dedito182@gmail.com>
2021-09-29 08:41:23 -0300

    creating list with react

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
-import Membro from './components/Membro';
+import Feed from './components/feed';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Membro nome="Visitante"/>
-      </div>
-    )
-  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      feed: [
+        {id: 1, username: 'john wick', curtidas: 10, comentarios: 2},
+        {id: 2, username: 'scooby-doo', curtidas: 210, comentarios: 24},
+        {id: 3, username: 'salsicha', curtidas: 10, comentarios: 184},
+        {id: 3, username: 'travis barker', curtidas: 400, comentarios: 7548}
+      ]
+    }
+   }
+
+render() {
+  return(
+    <div>
+       {this.state.feed.map((item) => {
+        return(
+          <Feed id={item.id} 
+          username={item.username}
+          curtidas={item.curtidas}
+          comentarios={item.comentarios}/>
+        );
+      })}
+      
+    </div>
+  )
+}
 }
 
 export default App;
